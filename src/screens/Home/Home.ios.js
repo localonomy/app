a6bc4a623bbbfc319bc67cc1c18db44cee906df4
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-05-31 13:43:40 +0200

    Populate home screen

--- src/screens/Home/Home.ios.js
+++ src/screens/Home/Home.ios.js
@@ -1,7 +1,14 @@
 // @flow
 
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import {
+  Alert,
+  Picker,
+  TextInput,
+  View,
+} from 'react-native'
+
+import Tabs from 'react-native-tabs'
 
 import store from './../../store'
 
@@ -10,13 +17,42 @@ import Title from './../../components/Title'
 
 import styles from './styles'
 
+const CountryPicker = ({ countries }) => (
+  <Picker
+    style={{width: 500}}
+    onValueChange={(country) => Alert.alert(
+      'Selected Country',
+      country
+    )}>
+    <Picker.Item label='Please select a country' value='' />
+    {countries.map((country) => {
+      return (
+        <Picker.Item
+          key={country.id}
+          label={country.name}
+          value={country.code}
+        />
+      )
+    })}
+  </Picker>
+)
+
+const DishTextInput= () => (
+  <TextInput
+    style={{height: 60}}
+    placeholder='Please type a dish name'
+    returnKeyType='go'
+  />
+)
+
 export default class Home extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
 
     this.state = {
       countries: [],
       filters: [],
+      tab: 'country',
     }
   }
 
@@ -24,21 +60,38 @@ export default class Home extends Component {
     let countries = await store.get('countries')
     let filters = await store.get('filters')
 
-    this.setState({
+    this.setState((prev) => ({
+      ...prev,
       countries,
       filters,
-    })
+    }))
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Filters: {this.state.filters[0]}, {this.state.filters[1]}, {this.state.filters[2]}
-        </Text>
-        <Text>
-          Countries: {this.state.countries[0] && this.state.countries[0].name}, {this.state.countries[1] && this.state.countries[1].name}, {this.state.countries[2] && this.state.countries[2].name}
-        </Text>
+        <Title>Discover Local Delicacies!</Title>
+        <Tabs
+          selected={this.state.tab}
+          onSelect={(el) => {
+            this.setState((prev) => ({ ...prev, tab: el.props.name })) 
+          }}
+        >
+          <Text name='country'>By Country</Text>
+          <Text name='dish'>By Dish Name</Text>
+        </Tabs>
+        {(this.state.tab === 'country') ?
+          <CountryPicker countries={this.state.countries} /> : 
+          <DishTextInput />
+        }
+
+        {this.state.filters.map((filter) => {
+          return (
+            <Text key={filter}>
+              {filter}
+            </Text>
+          )
+        })}
       </View>
     )
   }
