5ebfd679e9f9f02706c8b85813c052bd7aa57d21
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-05-31 14:06:11 +0200

    Implement basic cache in store

--- src/store/index.js
+++ src/store/index.js
@@ -8,19 +8,15 @@ const store = {
   async get(key) {
     let value
 
-    // Try retrieving the value from local storage
-    value = await ReactNativeStore.get(key)
+    let cacheValidUntil = await ReactNativeStore.get(`${key}-cache`)
+    let isCacheValid = cacheValidUntil && new Date() < new Date(cacheValidUntil)
 
     // Hit, retrieve from local storage
-    if (value != null) {
-      /* -- TODO --
-       * Check the validity of the value with some mechanism to be defined!
-       */
-
-      // Return the value
-      return new Promise((resolve, reject) => { resolve(value) })
+    if (isCacheValid) {
+      // Try retrieving the value from local storage
+      value = await ReactNativeStore.get(key)  
     }
-
+ 
     // Miss, try retrieving the value from API
     else {
       // Try retrieving the value from API
@@ -28,10 +24,14 @@ const store = {
 
       // Save the value in local storage
       ReactNativeStore.save(key, value)
-
-      // Return the value
-      return new Promise((resolve, reject) => { resolve(value) })
+      ReactNativeStore.save(
+        `${key}-cache`,
+        new Date(new Date().getTime() + (27 * 86400000)).toString() // 27 days
+      )
     }
+
+    // Return the value
+    return new Promise((resolve, reject) => { resolve(value) })
   }
 }
 
