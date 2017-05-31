9151f97383ae44262fdcce06954d2c9b360ad5d4
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-05-31 11:52:50 +0200

    Add store config and data

--- src/store/api.js
+++ src/store/api.js
@@ -1,13 +1,22 @@
 // @flow
 
+import config from './config'
+
+import badges from './data/badges.json'
+import filters from './data/filters.json'
+
 const get = async (key) => {
   switch (key) {
     case 'filters':
-      return new Promise((resolve, reject) => { resolve(['meat', 'spicy', 'gluten']) })
+      return new Promise((resolve, reject) => { resolve(filters) })
+      break
+
+    case 'badges':
+      return new Promise((resolve, reject) => { resolve(badges) })
       break
   
     case 'countries':
-      let response = await fetch(`http://localhost:3000/api/countries`)
+      let response = await fetch(`${config.url}/api/countries`)
       return response.json()
       break
 
---
+++ src/store/config.js
@@ -0,0 +1,5 @@
+// @flow
+
+export default {
+  url: `http://localhost:3000`,
+}
