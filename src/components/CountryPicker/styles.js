7b2882f25ea66378745cd1a18c34fa957cde5fc9
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-06-07 17:49:01 +0200

    Tweak picker list height ... with more magic numbers

--- src/components/CountryPicker/styles.js
+++ src/components/CountryPicker/styles.js
@@ -15,7 +15,10 @@ export default StyleSheet.create({
   list: {
     backgroundColor : "#F5FCFF",
     borderColor: 'grey',
-    height: (279/667) * height, // TODO - Magic numbers ...
+    height: // Magic numbers ...
+      (height === 667) ? 279 : // iPhone 6 - iPhone 6s - iPhone 7
+      (height === 736) ? 348 : // iPhone 6 Plus - iPhone 6s Plus - iPhone 7 Plus
+      300, // This could be just about anything ¯\_(ツ)_/¯
     width: width - (12 * 2),
   },
   backdrop: {
