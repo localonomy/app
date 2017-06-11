0890d5b6007fb7bd4642b0b29e93ec42c345ec38
Antonio Villagra De La Cruz <antonio.villagra.de.la.cruz@gmail.com>
2017-06-11 22:30:27 +0200

    Correct opacity flashing when pressing filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -3,7 +3,8 @@
 import React from 'react'
 import {
   Image,
-  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
 } from 'react-native'
 
 import config from './../../config'
@@ -11,15 +12,17 @@ import config from './../../config'
 import styles from './styles'
 
 const Filter = ({ filter, disabled, onPress, style }) => (
-  <TouchableOpacity style={[styles.filter]} onPress={onPress}>
-    <Image
-      style={[styles.image, disabled && styles.disabled, style]}
-      source={{
-        uri: `${config.url}/img/filter/${filter}.png`,
-        cache: 'force-cache',
-      }}
-    />
-  </TouchableOpacity>
+  <TouchableWithoutFeedback style={[styles.filter]} onPress={onPress}>
+    <View>
+      <Image
+        style={[styles.image, disabled && styles.disabled, style]}
+        source={{
+          uri: `${config.url}/img/filter/${filter}.png`,
+          cache: 'force-cache',
+        }}
+      />
+    </View>
+  </TouchableWithoutFeedback>
 )
 
 export default Filter
